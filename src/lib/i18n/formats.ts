e1ff7b8446c5d7ee87bd43c58cd5f84ce0768092
papotte <berm.ross@gmail.com>
2025-08-06 16:30:50 +0200

    hotfix: hardcode defaultTimezone

--- src/lib/i18n/formats.ts
+++ src/lib/i18n/formats.ts
@@ -1,6 +1,5 @@
 import { Formats } from 'use-intl'
 
-const defaultTimezone = 'Europe/Berlin'
 export const formattingProps: Formats = {
   dateTime: {
     long: {
@@ -8,38 +7,30 @@ export const formattingProps: Formats = {
       day: 'numeric',
       year: 'numeric',
       weekday: 'long',
-      timeZone: defaultTimezone,
     },
     short: {
       month: 'numeric',
       day: 'numeric',
       year: 'numeric',
-      timeZone: defaultTimezone,
     },
     shortTime: {
       hour: '2-digit',
       minute: '2-digit',
       hour12: false,
-      timeZone: defaultTimezone,
     },
     month: {
       month: 'long',
-      timeZone: defaultTimezone,
     },
     monthYear: {
       month: 'long',
       year: 'numeric',
-      timeZone: defaultTimezone,
     },
     yearMonth: {
       year: 'numeric',
       month: 'numeric',
-      timeZone: defaultTimezone,
-
     },
     weekday: {
       weekday: 'short',
-      timeZone: defaultTimezone,
     },
   },
   number: {
