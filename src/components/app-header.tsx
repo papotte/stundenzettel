ecbd51d0002cd98697cf1abb4e4036943d33b7ad
papotte <berm.ross@gmail.com>
2026-02-15 19:02:18 +0100

    fix: bar should be white

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -13,7 +13,7 @@ export default function AppHeader() {
 
   return (
     <header
-      className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6"
+      className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-white px-4 md:px-6"
       role="banner"
     >
       <Link
